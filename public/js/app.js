229d1388463ab8c61920493f01f72f844935933e
Daniel Paul Tchatchouang Ouokam <tchatchouang.ouokam@gmail.com>
2021-03-02 11:19:30 +0100

    adding UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,30 @@
 console.log("Client side javascript file is loaded!");
+const search = document.querySelector(".input__search");
+const form = document.querySelector(".search__form");
+const img = document.querySelector(".weather__img");
+const text = document.querySelector(".answer");
+const locationUI = document.querySelector(".location");
+const weatherLocation = async function (address) {
+  try {
+    text.textContent = "loading...";
+    img.setAttribute("src", "");
+    const res = await fetch(`http://localhost:3000/weather?address=${address}`);
+    const data = await res.json();
+
+    if (data.error) {
+      text.textContent = data.error;
+      throw new error("invalid location");
+    }
+    img.setAttribute("src", data.weatherIcons);
+    locationUI.textContent = data.location;
+    text.textContent = data.forecast;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  weatherLocation(search.value);
+});
+//weatherLocation("!");
